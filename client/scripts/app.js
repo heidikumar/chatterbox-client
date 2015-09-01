be6d655bdf21054ad01ef5a0df339e2dccf1964a
heidikumar <heidi.kristine.hansen@gmail.com>
2015-08-31 18:46:42 -0700

    Adding in .init, .send and initial .fetch.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,17 +1,17 @@
 var app = {
 
 };  
-// YOUR CODE HERE:
-$(document).ready(function(){
 
-//function to send messages
+app.init = function(){
 
-$('.send').on('click', function(){
-  // console.log('button working');
-  var message = {};
-  message.username = "Heidi and Tad's Excellent Adventures"
-  message.text = "Please don't hack us!"
-  message.room = "The Floor of Awesomeness"
+};
+
+app.send = function(message){
+
+  // var message = {};
+  // message.username = "Heidi and Tad's Excellent Adventures"
+  // message.text = "Please don't hack us!"
+  // message.room = "The Floor of Awesomeness"
     $.ajax({
     // This is the url you should use to communicate with the parse API server.
     url: 'https://api.parse.com/1/classes/chatterbox',
@@ -26,39 +26,61 @@ $('.send').on('click', function(){
       console.error('chatterbox: Failed to send message');
     }
   });
-})
+};
 
-//function to get messages
-  //this needs to be within a function, but for now we are putting it in a button for testing
+app.fetch = function(){
 
-$('.get').on('click', function(){
+};
 
-    $.ajax({
-    // This is the url you should use to communicate with the parse API server.
-    url: 'https://api.parse.com/1/classes/chatterbox',
-    type: 'GET',
-    contentType: 'application/json',
-    success: function (data) {
-      console.log('We got data back');
-    },
-    error: function (data) {
-      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-      console.log('failed to get anything');
-      // console.error('chatterbox: Failed to send message');
-    }
-  });
+// YOUR CODE HERE:
 
-})
+//THIS TOTALLY WORKS BUT IS NOT WHAT THEY ASKED FOR!!!!
 
+// $(document).ready(function(){
 
-//function to get messages
+// //function to send messages
 
+// $('.send').on('click', function(){
+//   // console.log('button working');
+//   var message = {};
+//   message.username = "Heidi and Tad's Excellent Adventures"
+//   message.text = "Please don't hack us!"
+//   message.room = "The Floor of Awesomeness"
+//     $.ajax({
+//     // This is the url you should use to communicate with the parse API server.
+//     url: 'https://api.parse.com/1/classes/chatterbox',
+//     type: 'POST',
+//     data: JSON.stringify(message),
+//     contentType: 'application/json',
+//     success: function (data) {
+//       console.log('chatterbox: Message sent');
+//     },
+//     error: function (data) {
+//       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//       console.error('chatterbox: Failed to send message');
+//     }
+//   });
+// })
 
+// //function to get messages
+//   //this needs to be within a function, but for now we are putting it in a button for testing
 
+// $('.get').on('click', function(){
 
+//     $.ajax({
+//     // This is the url you should use to communicate with the parse API server.
+//     url: 'https://api.parse.com/1/classes/chatterbox',
+//     type: 'GET',
+//     contentType: 'application/json',
+//     success: function (data) {
+//       console.log('We got data back');
+//     },
+//     error: function (data) {
+//       // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+//       console.log('failed to get anything');
+//       // console.error('chatterbox: Failed to send message');
+//     }
+//   });
 
-
-
-
-
-});
+// })
+// });
